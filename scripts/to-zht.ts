7564b5c9d361cbb526a93beaf71154877097cc0f
bluelovers <codelovers@users.sourceforge.net>
2022-01-09 08:43:13 +0800

    .

--- scripts/to-zht.ts
+++ scripts/to-zht.ts
@@ -10,6 +10,7 @@ import { join } from 'upath2';
 import { console } from 'debug-color2';
 import { cli_logger } from '../lib/cli-progress';
 import { processText } from 'novel-segment-cli';
+import { chkcrlf, CR, LF, CRLF } from 'crlf-normalize';
 
 cli_logger(FastGlob([
 	'**/*',
@@ -20,13 +21,16 @@ cli_logger(FastGlob([
 	{
 		let fullpath_new = join(__plugin_dev_dir, file);
 
-		if (!/\.(png)$/i.test(file))
+		if (!/\.(png)$|MANIFEST\.MF/i.test(file))
 		{
 			let fullpath = join(__plugin_downloaded_dir_unzip, file);
-
 			const content_old = await readFile(fullpath).then(content => content.toString());
+
+			let _lb = chkcrlf(content_old);
+
 			let content_new = await processText(content_old, {
 				convertToZhTw: true,
+				crlf: _lb.crlf ? CRLF : (_lb.lf || !_lb.cr) ? LF : CR,
 			});
 
 			if (/META-INF\/plugin\.xml$/i.test(file))
