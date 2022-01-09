bd0e8fd792b2d475097ae1d0df975ebf928b8d9c
bluelovers <codelovers@users.sourceforge.net>
2022-01-10 07:30:51 +0800

    docs: 修改描述

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # README.md
 
-    
+    Chinese (Traditional) Language Pack / 繁體中文語言包
+
+The Chinese Language Pack localizes the UI of IntelliJ IDEA, AppCode, CLion, DataGrip, GoLand, PyCharm, PhpStorm, RubyMine, and WebStorm into Chinese.
+
+中文語言包將為您的 IntelliJ IDEA, AppCode, CLion, DataGrip, GoLand, PyCharm, PhpStorm, RubyMine, 和WebStorm 帶來中文化的界面。
 
 ## install
 
-```bash
-yarn add idea-l10n-zht
-yarn-tool add idea-l10n-zht
-yt add idea-l10n-zht
-```
+- install from (github)[https://github.com/bluelovers/idea-l10n-zht/raw/master/plugin-dev-out/zh.jar]
+- install from jetbrains
 
+> 注意：請先移除或禁用官方簡體中文語言包
--- scripts/to-zht.ts
+++ scripts/to-zht.ts
@@ -64,8 +64,9 @@ export default FastGlob<string>([
 						if (/META-INF\/plugin\.xml$/i.test(file))
 						{
 							content_new = content_new
-								.replace(/<name>.+<\/name>/, `<name>Chinese (Traditional) Language Pack / 中文語言包</name>`)
+								.replace(/<name>.+<\/name>/, `<name>Chinese (Traditional) Language Pack / 繁體中文語言包</name>`)
 								.replace(/<vendor>.+<\/vendor>/, `<vendor>bluelovers</vendor>`)
+								.replace(/<description>.+<\/description>/, `<description><![CDATA[The Chinese Language Pack localizes the UI of IntelliJ IDEA, AppCode, CLion, DataGrip, GoLand, PyCharm, PhpStorm, RubyMine, and WebStorm into Chinese.<br><br>中文語言包將為您的 IntelliJ IDEA, AppCode, CLion, DataGrip, GoLand, PyCharm, PhpStorm, RubyMine, 和WebStorm 帶來中文化的界面。]]></description>`)
 							;
 						}
 
