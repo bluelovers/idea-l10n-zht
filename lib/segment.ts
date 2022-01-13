5ded909ee747522b6c947f9469c1f76916642797
bluelovers <codelovers@users.sourceforge.net>
2022-01-14 05:40:11 +0800

    refactor: split to dict file

--- lib/segment.ts
+++ lib/segment.ts
@@ -3,6 +3,9 @@ import { chkcrlf, CR, CRLF, LF } from 'crlf-normalize';
 import { EnumDictDatabase } from "novel-segment/lib/const";
 import Bluebird from 'bluebird';
 import Segment from "novel-segment/lib";
+import { load as loadSynonym } from '@novel-segment/loaders/segment/synonym';
+import { load as loadTable } from '@novel-segment/loaders/segment/index';
+import { __dict_synonym_txt, __dict_table_txt } from './const';
 
 let inited: Segment;
 
@@ -15,7 +18,7 @@ export function initIdeaSegmentText()
 			{
 				inited = await getSegment({
 					//disableCache: true,
-				}).then(segment =>
+				}).then(async (segment) =>
 				{
 					let db_dict = segment.getDictDatabase(EnumDictDatabase.TABLE);
 					let db_synonym = segment.getDictDatabase(EnumDictDatabase.SYNONYM);
@@ -23,14 +26,31 @@ export function initIdeaSegmentText()
 					const autoCjk = db_dict.options.autoCjk;
 					db_dict.options.autoCjk = true;
 
+					/*
 					db_dict
 						.add(['選項卡', 0x100000, 0])
 						.add(['標籤頁', 0x100000, 0])
 						.add(['標簽頁', 0x100000, 0])
 					;
+					 */
+
+					await loadTable(__dict_table_txt)
+						.each(data =>
+						{
+							return db_dict.add(data as any);
+						})
+					;
 
 					db_dict.options.autoCjk = autoCjk;
 
+					await loadSynonym(__dict_synonym_txt)
+						.each(data =>
+						{
+							return db_synonym.add(data as any);
+						})
+					;
+
+					/*
 					db_synonym
 						.add(['頁籤', '選項卡', '標籤頁', '標簽頁', '选项卡', '标签页', '标签页'])
 						.add(['視窗', '窗口', '窗口'])
@@ -44,7 +64,9 @@ export function initIdeaSegmentText()
 						.add(['選單', '菜單', '菜单'])
 						.add(['日誌', '日志'])
 						.add(['註解', '注解', '注解'])
+						.add(['磁碟', '磁盤', '磁盘'])
 					;
+					 */
 
 					return inited = segment;
 				})
