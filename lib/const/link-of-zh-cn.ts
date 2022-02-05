e068566382ef996682a8028eac8b3798fbed9a6c
bluelovers <codelovers@users.sourceforge.net>
2022-02-05 08:20:01 +0800

    build(release): update build

--- lib/const/link-of-zh-cn.ts
+++ lib/const/link-of-zh-cn.ts
@@ -1,8 +1,8 @@
 
 
-export const __plugin_zh_cn_id = '154222';
+export const __plugin_zh_cn_id = '156292';
 
-export const __plugin_zh_cn_version = '213.284';
+export const __plugin_zh_cn_version = '221.114';
 
-export const __plugin_zh_cn_download = 'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=154222';
+export const __plugin_zh_cn_download = 'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=156292';
 
