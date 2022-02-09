81bae328e88cda5709c4cb44aa8fd7fc65d12f4e
bluelovers <codelovers@users.sourceforge.net>
2022-02-10 04:23:14 +0800

    ci: 當編譯檔案沒有變化時不產生 CHANGELOG.md

---
+++ scripts/ci-postpublish.ts
@@ -0,0 +1,94 @@
+import { gitDiffFrom } from 'git-diff-from';
+import { __root } from '../test/__root';
+import { gitlog } from 'gitlog2';
+import Bluebird from 'bluebird';
+import { not } from 'micromatch';
+import { updateChangelogByCwd } from '@yarn-tool/changelog';
+import { console } from 'debug-color2';
+import { crossSpawnGitSync, crossSpawnGitAsync, ISpawnGitAsyncOptions, ISpawnGitSyncOptions } from '@git-lazy/spawn';
+
+export default Bluebird.resolve((process.env as any).GITHUB_SHA as string)
+	.then((from) =>
+	{
+		from ||= 'origin/master';
+
+		//from = '2d01cffc5da15e0a34a40b40ec3b7d0cc7612dda';
+
+		let to = gitlog({
+			repo: __root,
+			cwd: __root,
+			number: 5,
+		})[0].hash;
+
+		console.log(`input`);
+
+		console.dir({
+			from,
+			to,
+		});
+
+		return gitDiffFrom(from, to, {
+			cwd: __root,
+		})
+	})
+	.then(async (info) =>
+	{
+
+		const files = not(info.files, [
+			'CHANGELOG.md',
+			'test/**',
+			'.run/**',
+			'docs/**',
+			'coverage/**',
+			'test/**',
+			'*.*',
+			'.*',
+		]);
+
+		//console.dir(files);
+		//console.dir(info);
+
+		console.log(`result`);
+
+		const result = {
+			from: info.from,
+			to: info.to,
+			changed: files.length,
+		};
+
+		console.dir(result);
+
+		if (info.from === info.to)
+		{
+			console.error(`git 沒有變化 或 遠端與本地無法比對`)
+			return
+		}
+
+		if (files.length)
+		{
+			if (files.length < 10)
+			{
+				console.log(`files`);
+				console.dir(files);
+			}
+
+			await updateChangelogByCwd(__root, {
+				type: 'independent',
+			});
+
+			await crossSpawnGitAsync('git', [
+				'commit',
+				'-m',
+				`build(changelog): update CHANGELOG`,
+				'./CHANGELOG.md',
+			], {
+				cwd: __root,
+				throwError: true,
+				printStderr: true,
+			});
+		}
+
+		return result
+	})
+//.tap(console.dir)
+;
