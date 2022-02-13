bf74f2cf75cc78e0b3f6f1a12897e9ee63045546
bluelovers <codelovers@users.sourceforge.net>
2022-02-13 14:46:08 +0800

    ci: 只有 CHANGELOG.md 變化時才更新 git tag

--- scripts/update-git-tag.ts
+++ scripts/update-git-tag.ts
@@ -1,19 +1,37 @@
-
 import { gitTag, gitTagSync, IOptions } from '@git-lazy/tag';
 import { findRoot, findRootLazy } from '@yarn-tool/find-root';
 import Bluebird from 'bluebird';
 import { __plugin_zh_cn_version } from '../lib/const/link-of-zh-cn';
+import { __root } from '../test/__root';
+import { gitlog } from 'gitlog2';
+import micromatch, { not, match } from 'micromatch';
 
 export default Bluebird.resolve()
-	.then(() => {
-		return findRootLazy()
-	})
-	.then((rootData) => {
-		return gitTag(`v${__plugin_zh_cn_version}`, {
-			cwd: rootData.root,
-			forceGitTag: true,
-		}, {
-			printStderr: true,
+	.then(() =>
+	{
+		return gitlog({
+			repo: __root,
+			cwd: __root,
+			number: 1,
+			execOptions: {
+				// 防止 git ENOBUFS 錯誤
+				// https://www.cxyzjd.com/article/F_Origin/108589968
+				maxBuffer: 1024 * 1024 * 100,
+			},
 		})
+	})
+	.then((logs) =>
+	{
+		if (match(logs[0].files, 'CHANGELOG.md').length)
+		{
+			return gitTag(`v${__plugin_zh_cn_version}`, {
+				cwd: __root,
+				forceGitTag: true,
+			}, {
+				printStderr: true,
+			})
+		}
+
+		console.warn(`略過本次 git tag 更新`)
 	})
 ;
