bbf480cd81cf876e641b26165baf6f4d795d5e2f
bluelovers <codelovers@users.sourceforge.net>
2022-04-06 00:54:22 +0800

    ci: update CHANGELOG 判定

--- scripts/ci-postpublish.ts
+++ scripts/ci-postpublish.ts
@@ -20,20 +20,31 @@ export default Bluebird.resolve((process.env as any).GITHUB_SHA as string)
 
 		//from = '2d01cffc5da15e0a34a40b40ec3b7d0cc7612dda';
 
-		let to = getGitLogs()[0].hash;
+		const latestLog = getGitLogs()[0]
+
+		let to = latestLog.hash;
 
 		console.log(`input`);
 
 		console.dir({
 			from,
 			to,
+			head: {
+				subject: latestLog.subject,
+			},
 		});
 
-		return gitDiffFrom(from, to, {
-			cwd: __root,
+		return Bluebird.props({
+			info: gitDiffFrom(from, to, {
+				cwd: __root,
+			}),
+			latestLog,
 		})
 	})
-	.then(async (info) =>
+	.then(async ({
+		info,
+		latestLog,
+	}) =>
 	{
 
 		const files = not(info.files, [
@@ -52,30 +63,64 @@ export default Bluebird.resolve((process.env as any).GITHUB_SHA as string)
 
 		console.log(`result`);
 
+		const isSameHash = info.from === info.to;
+		const isBuildCommit = latestLog.subject.startsWith('build(release): update build');
+
 		const result = {
 			from: info.from,
 			to: info.to,
 			changed: files.length,
+			isSameHash,
+			isBuildCommit,
 		};
 
 		console.dir(result);
 
-		if (info.from === info.to)
+		if (isSameHash)
 		{
-			console.error(`git 沒有變化 或 遠端與本地無法比對`)
-			return
-		}
+			console.error(`git 沒有變化 或 遠端與本地無法比對差異`)
 
-		if (files.length && micromatch(files, [
-			'plugin-dev-out/*.jar',
-		]).length)
-		{
-			if (files.length < 10)
+			if (!isBuildCommit)
 			{
-				console.log(`files`);
-				console.dir(files);
+				return
 			}
 
+			console.yellow.warn(`前次任務可能未正確執行 CHANGELOG，嘗試檢測差異列表`)
+		}
+
+		let _do = await Promise.resolve().then(() =>
+		{
+			let _files: string[];
+
+			if (files.length && micromatch(files, [
+				'plugin-dev-out/*.jar',
+			]).length)
+			{
+				_files = files
+			}
+			else if (isBuildCommit && !files.length && micromatch(latestLog.files, [
+				'plugin-dev-out/*.jar',
+			]).length)
+			{
+				_files = latestLog.files
+			}
+
+			if (typeof _files?.length === 'number')
+			{
+				if (_files.length < 10)
+				{
+					console.log(`files`);
+					console.dir(_files);
+				}
+
+				return true
+			}
+
+			return false;
+		});
+
+		if (_do)
+		{
 			await updateChangelogByCwd(__root, {
 				type: 'independent',
 			});
