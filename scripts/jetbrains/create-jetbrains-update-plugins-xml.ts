88a4cc94088158f207d005a557e62d2a83f03971
bluelovers <codelovers@users.sourceforge.net>
2022-05-31 03:58:39 +0800

    feat(jetbrains): 支援非最新版 IDE 版本

--- scripts/jetbrains/create-jetbrains-update-plugins-xml.ts
+++ scripts/jetbrains/create-jetbrains-update-plugins-xml.ts
@@ -1,7 +1,8 @@
 import Bluebird from 'bluebird';
-import { outputFile } from 'fs-extra';
+import { outputFile, readJSON } from 'fs-extra';
 import { join } from 'upath2';
 import { __root } from '../../test/__root';
+import { LF } from 'crlf-normalize';
 
 /**
  * @see https://plugins.jetbrains.com/docs/intellij/update-plugins-format.html#format-of-updatepluginsxml-file
@@ -11,27 +12,46 @@ import { __root } from '../../test/__root';
 export default Bluebird.resolve()
 	.then(async () =>
 	{
-		const { version_map_record } = await import('../../lib/const/version-map.json');
-		const { __plugin_zh_cn_version } = await import('../../lib/const/link-of-zh-cn');
+		const { version_map_record } = await readJSON(join(__root, 'lib/const/version-map.json')) as typeof import('../../lib/const/version-map')
 
-		const { since } = version_map_record[__plugin_zh_cn_version];
+		const { __plugin_zh_cn_version } = await import('../../lib/const/link-of-zh-cn');
 
 		const href = `https://github.com/bluelovers/idea-l10n-zht/raw/master/plugin-dev-out/zh.jar`;
 
-		return outputFile(join(__root, 'plugin-dev-out', 'updatePlugins.xml'), `<?xml version="1.0" encoding="UTF-8"?>
-<plugins>
-  <plugin
+		const lines: string[] = [];
+
+		lines.push(`<?xml version="1.0" encoding="UTF-8"?>`);
+		lines.push(`<plugins>`);
+
+		Object.entries(version_map_record)
+			.forEach(([version, data]) =>
+			{
+
+				const { since } = data;
+				let href = `https://github.com/bluelovers/idea-l10n-zht/raw/v${version}/plugin-dev-out/zh.jar`;
+
+				if (version === __plugin_zh_cn_version)
+				{
+					href = `https://github.com/bluelovers/idea-l10n-zht/raw/master/plugin-dev-out/zh.jar`;
+				}
+
+				lines.push(`  <plugin
     id="com.intellij.zh-tw"
     url="${href}"
-    version="${__plugin_zh_cn_version}">
+    version="${version}">
     <idea-version since-build="${since}" />
   </plugin>
   <plugin
     id="sc.plugin.com.intellij.zh-tw"
     url="${href}"
-    version="${__plugin_zh_cn_version}">
+    version="${version}">
     <idea-version since-build="${since}" />
-  </plugin>
-</plugins>`)
+  </plugin>`);
+
+			})
+
+		lines.push(`</plugins>`);
+
+		return outputFile(join(__root, 'plugin-dev-out', 'updatePlugins.xml'), lines.join(LF))
 	})
 ;
