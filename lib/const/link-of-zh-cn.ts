2050ff2cfc9fd263796f721bc743040e531ef34d
GitHub Action <action@github.com>
2022-07-06 04:19:44 +0000

    build(cache): update link-of-zh-cn.ts

--- lib/const/link-of-zh-cn.ts
+++ lib/const/link-of-zh-cn.ts
@@ -1,20 +1,20 @@
 
 /**
- * @default '177159'
+ * @default '191906'
  */
-export const __plugin_zh_cn_id: string = '177159';
+export const __plugin_zh_cn_id: string = '191906';
 
 /**
- * @default '222'
+ * @default '223'
  */
-export const __plugin_zh_cn_series: string = '222';
+export const __plugin_zh_cn_series: string = '223';
 /**
- * @default '222.84'
+ * @default '223.35'
  */
-export const __plugin_zh_cn_version: string = '222.84';
+export const __plugin_zh_cn_version: string = '223.35';
 
 /**
- * @see https://plugins.jetbrains.com/plugin/download?rel=true&updateId=177159
+ * @see https://plugins.jetbrains.com/plugin/download?rel=true&updateId=191906
  */
-export const __plugin_zh_cn_download: string = 'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=177159';
+export const __plugin_zh_cn_download: string = 'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=191906';
 
