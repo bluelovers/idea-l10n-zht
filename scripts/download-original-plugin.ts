71a7ac7eebd5ee03806715e25a8f4717de4481d7
bluelovers <codelovers@users.sourceforge.net>
2022-07-26 03:38:45 +0800

    refactor: 避免同時存在不同的下載邏輯與顯示方式

--- scripts/download-original-plugin.ts
+++ scripts/download-original-plugin.ts
@@ -1,11 +1,3 @@
-import { join } from 'upath2';
-import { cli_logger } from '../lib/cli-progress';
-import { __plugin_downloaded_dir } from '../lib/const';
-import { __plugin_zh_cn_download, __plugin_zh_cn_id, __plugin_zh_cn_version } from '../lib/const/link-of-zh-cn';
-import { downloadPlugin, generateDownloadMessage } from '../lib/util/download-plugin';
+import { internalDownload } from '../lib/cli/internalDownload';
 
-export default cli_logger(downloadPlugin(__plugin_zh_cn_download, join(__plugin_downloaded_dir, 'zh.zip')), generateDownloadMessage({
-	id: __plugin_zh_cn_id,
-	version: __plugin_zh_cn_version,
-}))
-;
+export default internalDownload();
