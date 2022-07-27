9f51d8f9b2c33ad1a7a7b13fb16dd302720a45ea
bluelovers <codelovers@users.sourceforge.net>
2022-07-28 00:34:35 +0800

    feat(git): 檢測目前分支名稱來判斷要下載的版本

--- scripts/download-original-plugin.ts
+++ scripts/download-original-plugin.ts
@@ -1,3 +1,20 @@
 import { internalDownload } from '../lib/cli/internalDownload';
+import Bluebird from 'bluebird';
+import { currentBranchName } from '@git-lazy/branch';
+import { __root } from '../test/__root';
 
-export default internalDownload();
+export default Bluebird.resolve()
+	.then(() =>
+	{
+		let name = currentBranchName(__root);
+
+		if (name?.length > 0)
+		{
+			let m = /^v(\d+)(?:\.(?:\d+))?$/.exec(name);
+
+			return internalDownload(m?.[1])
+		}
+
+		return internalDownload()
+	})
+;
