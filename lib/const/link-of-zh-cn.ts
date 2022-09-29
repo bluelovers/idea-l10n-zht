870adb2134df9026b8a9688cb40d9995541a23eb
bluelovers <codelovers@users.sourceforge.net>
2022-09-30 07:52:38 +0800

    build(cache): update link-of-zh-cn.ts

--- lib/const/link-of-zh-cn.ts
+++ lib/const/link-of-zh-cn.ts
@@ -1,9 +1,9 @@
 
 /**
- * @default '191906'
+ * @default '230892'
  * @deprecated
  */
-export const __plugin_zh_cn_id: string = '191906';
+export const __plugin_zh_cn_id: string = '230892';
 
 /**
  * @default '223'
@@ -11,14 +11,14 @@ export const __plugin_zh_cn_id: string = '191906';
  */
 export const __plugin_zh_cn_series: string = '223';
 /**
- * @default '223.35'
+ * @default '223.143'
  * @deprecated
  */
-export const __plugin_zh_cn_version: string = '223.35';
+export const __plugin_zh_cn_version: string = '223.143';
 
 /**
- * @see https://plugins.jetbrains.com/plugin/download?rel=true&updateId=191906
+ * @see https://plugins.jetbrains.com/plugin/download?rel=true&updateId=230892
  * @deprecated
  */
-export const __plugin_zh_cn_download: string = 'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=191906';
+export const __plugin_zh_cn_download: string = 'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=230892';
 
