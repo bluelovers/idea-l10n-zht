4e1097e5f3ad5f3bc40fdbc264874860b3cfc065
bluelovers <codelovers@users.sourceforge.net>
2022-12-13 04:25:51 +0800

    feat(cli): 在執行 cli 指令時同時檢查更新

--- bin/download-series.ts
+++ bin/download-series.ts
@@ -4,6 +4,12 @@ import { parseArgvDownload } from '../lib/cli/parse-argv-download';
 import { _handleArgv, _handleArgvResult } from '../lib/cli/handle-argv';
 
 export default parseArgvDownload()
+	.then(async _ => {
+
+		await import('../scripts/fetch-latest-version-of-zh-cn').then(m => m.default);
+
+		return _;
+	})
 	.then(_handleArgv)
 	.then(_handleArgvResult)
 ;
