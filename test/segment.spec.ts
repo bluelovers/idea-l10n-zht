ae8639e235743f3bfc146bc648406ac36cc099fc
bluelovers <codelovers@users.sourceforge.net>
2022-12-30 05:03:07 +0800

    refactor(dict): { 依赖 => 相依 }
    https://github.com/bluelovers/idea-l10n-zht/issues/81

--- test/segment.spec.ts
+++ test/segment.spec.ts
@@ -328,6 +328,8 @@ describe(`should include`, () => {
 
 		[`主控台`, `action.IdeScriptingConsole.description=打开 IDE 脚本控制台`],
 
+		[`連線`, `there.are.not.selected.module.dependencies.of.the.module.br.b.0.b=没有选择模块的模块依赖项: <br><b>{0}</b>`],
+
 	]).sort((a, b) => {
 		return _comp(a[0], b[0])
 	}).forEach(text => {
